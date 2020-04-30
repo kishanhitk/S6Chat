062694148370b9d560cf771cec27b3043e9996d7
kishanhitk <kishansharma1231@gmail.com>
2020-04-30 07:50:37 +0530

    Fixed a major bug introduced in last commit - Database not creating for new user.

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -5,10 +5,16 @@ class DataBaseServices {
   DataBaseServices({this.uid});
   final CollectionReference _doc = Firestore.instance.collection('users');
   Future createUserDatabase(String name, String phoneNo) async {
-    return await _doc.document(uid).updateData({
-      'uid': uid,
-      'name': name,
-      'phoneNo': phoneNo,
-    });
+    return await _doc.document(uid) != null
+        ? _doc.document(uid).updateData({
+            'uid': uid,
+            'name': name,
+            'phoneNo': phoneNo,
+          })
+        : _doc.document(uid).setData({
+            'uid': uid,
+            'name': name,
+            'phoneNo': phoneNo,
+          });
   }
 }
